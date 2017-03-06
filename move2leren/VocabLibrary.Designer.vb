1e3fad8dfd0a3f273a51b587148a7e53ffc7e4ca
Tom Chate <up774061@myport.ac.uk>
2017-03-06 16:37:22 +0000

    updates

--- move2leren/VocabLibrary.Designer.vb
+++ move2leren/VocabLibrary.Designer.vb
@@ -24,9 +24,12 @@ Partial Class VocabLibrary
     Private Sub InitializeComponent()
         Me.Label1 = New System.Windows.Forms.Label()
         Me.TabControl = New System.Windows.Forms.TabControl()
-        Me.TabPage1 = New System.Windows.Forms.TabPage()
+        Me.TabPageShopping = New System.Windows.Forms.TabPage()
         Me.TabPage2 = New System.Windows.Forms.TabPage()
+        Me.Button6 = New System.Windows.Forms.Button()
+        Me.ListBoxVocab = New System.Windows.Forms.ListBox()
         Me.TabControl.SuspendLayout()
+        Me.TabPageShopping.SuspendLayout()
         Me.SuspendLayout()
         '
         'Label1
@@ -42,12 +45,12 @@ Partial Class VocabLibrary
         'TabControl
         '
         Me.TabControl.Alignment = System.Windows.Forms.TabAlignment.Left
-        Me.TabControl.Controls.Add(Me.TabPage1)
+        Me.TabControl.Controls.Add(Me.TabPageShopping)
         Me.TabControl.Controls.Add(Me.TabPage2)
         Me.TabControl.DrawMode = System.Windows.Forms.TabDrawMode.OwnerDrawFixed
         Me.TabControl.Font = New System.Drawing.Font("Tahoma", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
         Me.TabControl.ItemSize = New System.Drawing.Size(30, 120)
-        Me.TabControl.Location = New System.Drawing.Point(137, 134)
+        Me.TabControl.Location = New System.Drawing.Point(144, 126)
         Me.TabControl.Multiline = True
         Me.TabControl.Name = "TabControl"
         Me.TabControl.RightToLeft = System.Windows.Forms.RightToLeft.No
@@ -56,15 +59,16 @@ Partial Class VocabLibrary
         Me.TabControl.SizeMode = System.Windows.Forms.TabSizeMode.Fixed
         Me.TabControl.TabIndex = 9
         '
-        'TabPage1
+        'TabPageShopping
         '
-        Me.TabPage1.Location = New System.Drawing.Point(124, 4)
-        Me.TabPage1.Name = "TabPage1"
-        Me.TabPage1.Padding = New System.Windows.Forms.Padding(3)
-        Me.TabPage1.Size = New System.Drawing.Size(798, 556)
-        Me.TabPage1.TabIndex = 0
-        Me.TabPage1.Text = "Shopping"
-        Me.TabPage1.UseVisualStyleBackColor = True
+        Me.TabPageShopping.Controls.Add(Me.ListBoxVocab)
+        Me.TabPageShopping.Location = New System.Drawing.Point(124, 4)
+        Me.TabPageShopping.Name = "TabPageShopping"
+        Me.TabPageShopping.Padding = New System.Windows.Forms.Padding(3)
+        Me.TabPageShopping.Size = New System.Drawing.Size(798, 556)
+        Me.TabPageShopping.TabIndex = 0
+        Me.TabPageShopping.Text = "Shopping"
+        Me.TabPageShopping.UseVisualStyleBackColor = True
         '
         'TabPage2
         '
@@ -76,23 +80,46 @@ Partial Class VocabLibrary
         Me.TabPage2.Text = "Travel"
         Me.TabPage2.UseVisualStyleBackColor = True
         '
+        'Button6
+        '
+        Me.Button6.Font = New System.Drawing.Font("Tahoma", 13.8!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Button6.Location = New System.Drawing.Point(12, 696)
+        Me.Button6.Name = "Button6"
+        Me.Button6.Size = New System.Drawing.Size(126, 40)
+        Me.Button6.TabIndex = 15
+        Me.Button6.Text = "Return"
+        Me.Button6.UseVisualStyleBackColor = True
+        '
+        'ListBoxVocab
+        '
+        Me.ListBoxVocab.FormattingEnabled = True
+        Me.ListBoxVocab.ItemHeight = 24
+        Me.ListBoxVocab.Location = New System.Drawing.Point(112, 80)
+        Me.ListBoxVocab.Name = "ListBoxVocab"
+        Me.ListBoxVocab.Size = New System.Drawing.Size(501, 76)
+        Me.ListBoxVocab.TabIndex = 0
+        '
         'VocabLibrary
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 16.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
         Me.BackColor = System.Drawing.Color.White
         Me.ClientSize = New System.Drawing.Size(1167, 748)
+        Me.Controls.Add(Me.Button6)
         Me.Controls.Add(Me.TabControl)
         Me.Controls.Add(Me.Label1)
         Me.Name = "VocabLibrary"
         Me.Text = "VocabLibrary"
         Me.TabControl.ResumeLayout(False)
+        Me.TabPageShopping.ResumeLayout(False)
         Me.ResumeLayout(False)
         Me.PerformLayout()
 
     End Sub
     Friend WithEvents Label1 As System.Windows.Forms.Label
     Friend WithEvents TabControl As System.Windows.Forms.TabControl
-    Friend WithEvents TabPage1 As System.Windows.Forms.TabPage
+    Friend WithEvents TabPageShopping As System.Windows.Forms.TabPage
     Friend WithEvents TabPage2 As System.Windows.Forms.TabPage
+    Friend WithEvents Button6 As System.Windows.Forms.Button
+    Friend WithEvents ListBoxVocab As System.Windows.Forms.ListBox
 End Class
--- move2leren/VocabLibrary.vb
+++ move2leren/VocabLibrary.vb
@@ -1,13 +1,5 @@
 ﻿Public Class VocabLibrary
 
-    Private Sub Label1_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Label1.Click
-
-    End Sub
-
-    Private Sub TabPage1_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles TabPage1.Click
-
-    End Sub
-
     Private Sub TabControl1_DrawItem(ByVal sender As Object, ByVal e As System.Windows.Forms.DrawItemEventArgs) Handles TabControl.DrawItem
         Dim g As Graphics
         Dim sText As String
@@ -25,5 +17,15 @@
         iX = e.Bounds.Left + 6
         iY = e.Bounds.Top + (e.Bounds.Height - sizeText.Height) / 2
         g.DrawString(sText, ctlTab.Font, Brushes.Black, iX, iY) 'Code taken from http://arsalantamiz.blogspot.co.uk/2008/07/custom-tab-control-layout.html
+    End Sub
+
+    Private Sub Button6_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button6.Click
+        Me.Hide()
+        Home.Show()
+    End Sub
+
+    Private Sub TabPageShopping_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles TabPageShopping.Click
+        TabPageShopping.Text = "hello"
+        ListBoxVocab.Text = "hello"
     End Sub
 End Class
